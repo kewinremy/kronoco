b4eaa2d6775f7693ab2f527971c48ab458011a91
Kewin Rémy <kewin.remy@juliusbaer.com>
2022-03-23 14:17:40 +0100

    deploying a sample app with gitlab to k8s

---
+++ README.md
@@ -0,0 +1,9 @@
+# kronoco
+
+This repo contains a small application for gitlab demo purposes.
+
+It contains the default dotnet api, unit tests.
+
+## Getting started
+
+Clone the repo and have fun
---
+++ application/api.test/UnitTest.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace api.test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test()
+    {
+        Assert.Pass();
+    }
+}
